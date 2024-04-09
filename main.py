fc909c39a9326ce0ed0ec738180811394ae9d28b
Chris Strong <lupinedev@outlook.com>
2024-04-09 09:41:56 -0500

    Cleaned up code & made it use .env & added the original code file to the gitignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime as dt
-import os
 import pypco
+import datetime as dt
+
+import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -9,74 +10,75 @@ load_dotenv()
 # Get PCO account and secret from environment variables
 PCO_ACCOUNT = os.getenv("PCO_ACCOUNT")
 PCO_SECRET = os.getenv("PCO_SECRET")
-PCO_KEY = pypco.PCO(PCO_ACCOUNT, PCO_SECRET)
 
-# Set PCO API base URL
-pco_base_url = "https://api.planningcenteronline.com"
+pco = pypco.PCO(PCO_ACCOUNT, PCO_SECRET)
 
+print("--------------------------------")
+print("Checking for this week's plan...")
+print("--------------------------------")
 
-def main():
-    print("--------------------------------")
-    print("Checking for this week's plan...")
-    print("--------------------------------")
+nhe_plans = '/services/v2/service_types/95532/plans/'
+all_plans = pco.get(nhe_plans)
+number_of_plans = all_plans['meta']['total_count']
+offset = number_of_plans - 15
+offset_url = nhe_plans + '?offset=' + str(offset)
+get_plans = pco.get(offset_url)
 
-    # Base URL for all NHE Plans
-    nhe_plans = '/services/v2/service_types/95532/plans/'
+current = dt.datetime.now()
+year, month, day, hour, minute = current.year, current.month, current.day, current.hour, current.minute
 
-    # User input for the date
-    user_date = input("Enter the date (YYYY-MM-DD): ")
-    try:
-        date_obj = dt.datetime.strptime(user_date, "%Y-%m-%d")
-    except ValueError:
-        print("Invalid date format. Please enter date in YYYY-MM-DD format.")
-        return
+epoch_conversion_today = dt.datetime(year, month, day, 0, 0, 0).timestamp()
+current_time = dt.datetime(year, month, day, hour, minute, 0).timestamp()
 
-    # Convert user input date to ISO format
-    user_date_iso = date_obj.isoformat()
+service_id = get_plans['data'][0]['id']
 
-    # Get plans for the specified date
-    plans_for_date_response = PCO_KEY.get(f"{nhe_plans}?filter[future_or_past]=future&filter[range_start]={user_date_iso}&filter[range_end]={user_date_iso}")
+get_date = pco.get(nhe_plans + service_id)['data']['attributes']['sort_date']
+split_date = get_date.split("-")
+day_split = split_date[2].split("T")
+pco_date = dt.datetime(int(split_date[0]), int(split_date[1]), int(day_split[0]), 0, 0, 0).timestamp()
+this_week = pco_date + 600000
 
-    # Check if plans exist for the specified date
-    if not plans_for_date_response['data']:
-        print("No plans found for the specified date.")
-        return
+while epoch_conversion_today > this_week:
+    next_plan = pco.get(f'/services/v2/service_types/95532/plans/{service_id}/next_plan')['data']['id']
+    get_date = pco.get(nhe_plans + service_id)['data']['attributes']['sort_date']
+    split_date = get_date.split("-")
+    day_split = split_date[2].split("T")
+    pco_date = dt.datetime(int(split_date[0]), int(split_date[1]), int(day_split[0]), 0, 0, 0).timestamp()
+    this_week = pco_date + 600000
+    service_id = next_plan
 
-    print(f"Plan Date: {user_date}")
+service_link = nhe_plans + service_id
+service = pco.get(service_link)
+service_date = service['data']['attributes']['dates']
+
+is_correct = input(f"Is {service_date} the correct plan date? (y/n): ")
+if is_correct.lower() == "n":
+    desired_date = input("Enter the service date in YYYY-MM-DD format: ")
+    service_id = input("Please enter the service ID: ")
     print("---------------------------")
+    print("-------- Song Info --------")
+else:
+    print("---------------------------")
+    print("-------- Song Info --------")
 
-    # Loop through each plan for the specified date
-    for plan in plans_for_date_response['data']:
-        plan_id = plan['id']
-        # Retrieve song information for the plan
-        list_of_items = PCO_KEY.get(f"{nhe_plans}{plan_id}/items")['data']
-        
-        # Filter items to only include those for the specified date
-        items_for_date = [item for item in list_of_items if item['attributes'].get('dates') == user_date]
+list_of_items = pco.get(service_link + '/items')
+list_of_elements = list_of_items['data']
 
-        # Loop through each item in the plan for the specified date
-        for item in items_for_date:
-            # Check if the item is a song
-            if item['attributes']['item_type'] == "song":
-                try:
-                    song_id = item['relationships']['song']['data']['id']
-                    song_info = PCO_KEY.get(f'/services/v2/songs/{song_id}')
-                    song_attributes = song_info['data']['attributes']
+for song_index, song_item in enumerate(list_of_elements, start=1):
+    if song_item['attributes']['item_type'] == "song":
+        song_id = song_item['relationships']['song']['data']['id']
+        song_info = pco.get('/services/v2/songs/' + song_id)
+        song_name = song_info['data']['attributes']['title']
+        author = song_info['data']['attributes']['author']
+        copyright = song_info['data']['attributes']['copyright']
 
-                    song_name = song_attributes['title']
-                    author = song_attributes.get('author', '')
-                    song_copyright = song_attributes.get('copyright', '')
+        author_split = [a.strip(',') for a in author.split()] if author else []
+        copyright_split = copyright.split('(', 1)[0] if copyright else ''
 
-                    print("Title:", song_name)
-                    print("Author:", author)
-                    print("Copyright:", song_copyright)
-                    print("---------------------------")
+        song_file_name = f"song{song_index}.txt"
+        with open(song_file_name, "w") as f:
+            f.write(f"Title: {song_name}\n")
+            f.write(f"Author: {' '.join(author_split)}\n")
+            f.write(f"Copyright: {copyright_split}")
 
-                except Exception as e:
-                    print(f"Error processing song: {e}")
-
-    print("Song information has been displayed.")
-
-
-if __name__ == "__main__":
-    main()
+print("Song information saved successfully.")
